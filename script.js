fbc044bf93cca3410d60b5313d7ae7549c0fc5bd
Jeff Hughes <jeffhughes@Jeffs-Mac-mini-2.local>
2025-02-19 22:43:49 -0500

    added planet content and moons

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 function createStars() {
-  const container = document.querySelector("body");
-  for (let i = 0; i < 1000; i++) {
+  const container = document.querySelector("div.kuipers");
+  for (let i = 0; i < 5000; i++) {
     // Increase the number of stars to 1000
-    const star = document.createElement("div");
+    /*const star = document.createElement("div");
     star.className = "star";
     star.style.width = ".1px";
     star.style.height = ".1px";
     star.style.top = Math.random() * 100 + "%";
-    star.style.left = Math.random() * 100 + "%";
+    star.style.left = Math.random() * 100 + "%";*/
+
+    let angle = Math.random() * 6.28;
+
+    let radius = Math.random() * 50;
+    let xvalue = Math.cos(angle) * radius;
+    let yvalue = Math.sin(angle) * radius;
+
+    const star = document.createElement("div");
+
+    star.className = "star";
+    star.style.width = (Math.random() * 1) + "px";
+    star.style.height = (Math.random() * 1) + "px";
+    star.style.top = (50 + yvalue) + "%";
+    star.style.left = (50 + xvalue) + "%";
+
     container.appendChild(star);
   }
 }
@@ -56,6 +71,22 @@ function createKuipers() {
   }
 }
 
+function planetSelect(planet){
+  document.querySelectorAll("div.card").forEach(function(card){card.style="display:none;";});
+
+  document.querySelector("#" + planet + "-card").style="display:block;";
+}
+
+function changeAnimation(){
+  let animationButton = document.getElementById('animation-button');
+  document.querySelectorAll("div.orbit").forEach(function(orbitElement){
+    let playState = orbitElement.style.animationPlayState || 'running';
+    orbitElement.style.animationPlayState =  playState === 'running' ? 'paused' : 'running';
+    animationButton.innerText = ((playState == 'running') ? 'Resume': 'Pause');
+  });
+}
+
 createStars();
 createAsteroids();
 createKuipers();
+
